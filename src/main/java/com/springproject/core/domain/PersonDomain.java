dba1e92208d440c7877bd81b8c2460787a27bc37
Mateus Nunes Pinheiro <e-mateusp@weg.net>
2023-02-21 17:29:48 -0300

    create the class: BaseDoamin to Core

--- src/main/java/com/springproject/core/domain/PersonDomain.java
+++ src/main/java/com/springproject/core/domain/PersonDomain.java
@@ -2,39 +2,18 @@ package com.springproject.core.domain;
 
 import com.springproject.core.type.ActiveEnum;
 
-public class PersonDomain {
+public class PersonDomain extends BaseDomain {
 	public PersonDomain() {
 	}
 
-	public PersonDomain(Long id, String name, String email) {
-		this.id = id;
-		this.name = name;
+	public PersonDomain(Long id, String name, ActiveEnum isActive, String email) {
+		setId(id);
+		setName(name);
+		setIsActive(isActive);
 		this.email = email;
 	}
 
-	private Long id;
-
-	private String name;
-
 	private String email;
-
-	private ActiveEnum isActive;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 
 	public String getEmail() {
 		return email;
@@ -42,14 +21,6 @@ public class PersonDomain {
 
 	public void setEmail(String email) {
 		this.email = email;
-	}
-
-	public ActiveEnum getIsActive() {
-		return isActive;
-	}
-
-	public void setIsActive(ActiveEnum isActive) {
-		this.isActive = isActive;
 	}
 
 }
